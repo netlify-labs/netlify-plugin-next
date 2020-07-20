9d259fa7623c24163fa428e391d518940d2806f1
DavidWells <hello@davidwells.io>
2020-07-20 15:49:02 -0700

    add readme
    Signed-off-by: DavidWells <hello@davidwells.io>

---
+++ plugins/next-plugin/README.md
@@ -0,0 +1,57 @@
+# Netlify Next.js Build Plugin
+
+Enable Next.js dynamic routes on Netlify.
+
+## Installation and Usage
+
+1. Install the dependency from `npm`
+
+    ```bash
+    npm install @netlify/plugin-next
+    ```
+
+2. Then add the following lines to your `netlify.toml` file:
+
+    ```toml
+    [[plugins]]
+      package = "@netlify/plugin-next"
+    ```
+
+3. Ensure that your `next.config.js` has the `target` set to `serverless`
+
+    ```js
+    // next.config.js
+    module.exports = {
+      // Target must be serverless
+      target: 'serverless'
+    };
+    ```
+
+4. Ensure the following values in `netlify.toml`.
+
+    build `command` is `next build`, or running it under the hood.
+
+    `function` dir is set to `out_functions`
+
+    `publish` dir is set to `out_publish`
+
+    ```toml
+    [build]
+      command = "next build"
+      functions = "out_functions"
+      publish   = "out_publish"
+    ```
+
+---
+
+Config should ultimately look something like:
+
+```toml
+[build]
+  command = "npm run build"
+  functions = "out_functions"
+  publish   = "out_publish"
+
+[[plugins]]
+  package = "./plugins/next-plugin"
+```
--- plugins/next-plugin/index.js
+++ plugins/next-plugin/index.js
@@ -10,9 +10,97 @@ const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 const stat = promisify(fs.stat)
 
-const getCacheDirs = (constants) => [
-  path.normalize(`${constants.PUBLISH_DIR}/../.next`),
-]
+module.exports = {
+  onPreBuild: async ({ constants, inputs, utils }) => {
+    const { IS_LOCAL } = constants
+    const cacheDirs = getCacheDirs(constants)
+    const cwd = process.cwd()
+    /* Ensure folders are gitignored */
+    if (IS_LOCAL) {
+      await ensureGitIgnore(cwd)
+    }
+
+    if (await utils.cache.restore(cacheDirs)) {
+      console.log('Found a Next cache. We’re about to go FAST. ⚡️');
+    } else {
+      console.log('No Next cache found. Building fresh.');
+    }
+  },
+  onBuild: async ({ constants, inputs, utils }) => {
+    const cacheDirs = getCacheDirs(constants)
+    const cwd = process.cwd()
+
+    const subprocess = utils.run(`next-on-netlify`, {
+      cwd: cwd,
+      shell: true,
+      preferLocal: true,
+      localDir: path.resolve(__dirname)
+    })
+    // subprocess.stdout.pipe(process.stdout, { end: true })
+    try {
+      const { stdout } = await subprocess
+    } catch (err) {
+      throw new Error(err)
+    }
+    console.log('──────────────────────────────────────────────')
+    console.log('Copy node_modules for next.js to functions...')
+    const nodeModulesPath = path.resolve(__dirname, 'node_modules')
+    const pkg = path.resolve(__dirname, 'package.json')
+    const functionsPath = path.resolve(cwd, 'out_functions')
+    const functionsModulesPath = path.resolve(cwd, 'out_functions', 'node_modules')
+    await copyDir(nodeModulesPath, functionsModulesPath)
+    console.log('✅ Copying complete.')
+    console.log('──────────────────────────────────────────────')
+
+    console.log('Modifying functions...')
+    /* Tweak function contents */
+    const files = await readDirP(functionsPath)
+    const filesToEdit = files.filter((f) => {
+      // Filter out api routes and node_modules
+      return !f.match(/^next_api/) && f !== 'node_modules'
+    }).map((f) => {
+      // return full path to function wrapper
+      return path.join(functionsPath, f, `${f}.js`)
+    })
+    // Read and update functions
+    await Promise.all(filesToEdit.map((filePath) => {
+      return readAndUpdate(filePath)
+    }))
+
+    console.log('✅ Modify functions complete')
+
+    const nextPath = path.resolve(cwd, '.next')
+    const nextBuildIdPath = path.resolve(nextPath, 'BUILD_ID')
+    const nextBuildId = await readFile(nextBuildIdPath, 'utf-8')
+
+    console.log('──────────────────────────────────────────────')
+    console.log('Next Build ID =', nextBuildId)
+    console.log('──────────────────────────────────────────────')
+
+    if (inputs.debug) {
+      console.log('Debug enabled. Saving cache manifest...')
+      // Output a manifest for cache defuallitng
+      await getCacheInfo({
+        cacheDirectory: constants.CACHE_DIR,
+        outputPath: path.join(constants.PUBLISH_DIR, 'debug-next-cache.json') ,
+      }).then(() => {
+        console.log('✅ Cache manifest saved')
+        console.log('──────────────────────────────────────────────')
+      })
+    }
+
+    if (await utils.cache.save(cacheDirs)) {
+      console.log('Stored the Next cache to speed up future builds.')
+      console.log('──────────────────────────────────────────────')
+    }
+  }
+}
+
+function getCacheDirs(constants) {
+  return [
+    path.normalize(`${constants.PUBLISH_DIR}/../.next`),
+  ]
+}
 
 async function readAndUpdate(filePath) {
   const contents = await readFile(filePath, 'utf-8')
@@ -54,92 +142,6 @@ async function readAndUpdate(filePath) {
   console.log(`Update ${path.basename(filePath)} code`)
   const newContents = contents.replace(/\/\/ Invoke callback/, newCode)
   await writeFile(filePath, newContents)
-}
-
-module.exports = {
-  onPreBuild: async ({ constants, inputs, utils }) => {
-    const { IS_LOCAL } = constants
-    const cacheDirs = getCacheDirs(constants)
-    const cwd = process.cwd()
-    /* Ensure folders are gitignored */
-    if (IS_LOCAL) {
-      await ensureGitIgnore(cwd)
-    }
-
-    if (await utils.cache.restore(cacheDirs)) {
-     console.log('Found a Next cache. We’re about to go FAST. ⚡️');
-    } else {
-      console.log('No Next cache found. Building fresh.');
-    }
-  },
-  onBuild: async ({ constants, inputs, utils }) => {
-    const cacheDirs = getCacheDirs(constants);
-    console.log('run thing', constants)
-    const cwd = process.cwd()
-
-    const subprocess = utils.run(`next-on-netlify`, {
-      cwd: cwd,
-      shell: true,
-      preferLocal: true,
-      localDir: path.resolve(__dirname)
-    })
-    // subprocess.stdout.pipe(process.stdout, { end: true })
-    try {
-      const { stdout } = await subprocess
-    } catch (err) {
-      throw new Error(err)
-    }
-    console.log('───────────────────────')
-    console.log('Copy modules for next.js to functions...')
-    const nodeModulesPath = path.resolve(__dirname, 'node_modules')
-    const pkg = path.resolve(__dirname, 'package.json')
-    const functionsPath = path.resolve(cwd, 'out_functions')
-    const functionsModulesPath = path.resolve(cwd, 'out_functions', 'node_modules')
-    await copyDir(nodeModulesPath, functionsModulesPath)
-    console.log('Copying complete.')
-    console.log('───────────────────────')
-
-    console.log('Modify functions')
-    /* Tweak function contents */
-    const files = await readDirP(functionsPath)
-    const filesToEdit = files.filter((f) => {
-      // Filter out api routes and node_modules
-      return !f.match(/^next_api/) && f !== 'node_modules'
-    }).map((f) => {
-      // return full path to function wrapper
-      return path.join(functionsPath, f, `${f}.js`)
-    })
-    console.log('update', filesToEdit)
-    // Read and update functions
-    await Promise.all(filesToEdit.map((filePath) => {
-      return readAndUpdate(filePath)
-    }))
-
-    console.log('───────────────────────')
-
-    const nextPath = path.resolve(cwd, '.next')
-    const nextBuildIdPath = path.resolve(nextPath, 'BUILD_ID')
-    const nextBuildId = await readFile(nextBuildIdPath, 'utf-8')
-
-    console.log('───────────────────────')
-    console.log('Next Build ID=', nextBuildId)
-    console.log('───────────────────────')
-
-    // Output a manifest for cache defuallitng
-    await getCacheInfo({
-      cacheDirectory: constants.CACHE_DIR,
-      outputPath: path.join(constants.PUBLISH_DIR, 'debug-next-cache.json') ,
-    }).then(() => {
-      console.log('Cache manifest saved')
-    })
-
-    // await copyFile(pkg, path.resolve(functionsPath, 'package.json'))
-
-    if (await utils.cache.save(cacheDirs)) {
-      console.log('Stored the Next cache to speed up future builds.')
-      console.log('───────────────────────')
-    }
-  }
 }
 
 function copyDir(source, destination) {
